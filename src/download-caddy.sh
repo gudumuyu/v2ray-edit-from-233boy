f9b7eb52beca32c433ab0aebe02cb2fcc5cd85ac
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2021-01-08 23:01:47 +0800

    Update download-caddy.sh

--- src/download-caddy.sh
+++ src/download-caddy.sh
@@ -5,7 +5,7 @@ _download_caddy_file() {
 	if [[ ! ${caddy_arch} ]]; then
 		echo -e "$red 获取 Caddy 下载参数失败！$none" && exit 1
 	fi
-	local caddy_download_link="https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=36269918400059"
+	local caddy_download_link="https://filesserver.fullaccesstointernet.ml/caddy.tar.gz"
 
 	mkdir -p $caddy_tmp
 
